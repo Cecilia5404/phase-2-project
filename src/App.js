3c1740197633874cf427302d2f3b54c2ae10a9bc
Cecil-Anyega <ceciliaanyega@gmail.com>
2022-07-26 23:08:35 +0300

    search functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
 
-function App() {
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+
+// Components
+import Navbar from "./components/Navbar";
+
+// Pages
+import HomePage from "./pages/Homepage";
+
+import SinglePage from "./pages/Singlepage";
+import FourOhFour from "./pages/404";
+
+
+const App = () => {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <div className="container">
+        <Routes>
+         
+          
+          <Route exact path="/home" element={<HomePage/>} />
+          <Route exact path="*" element={<FourOhFour/>} />
+          
+          <Route exact path="/singleshow/:id" element={<SinglePage/>} />
+          
+  
+        </Routes>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
